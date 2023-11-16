eb42644feb3069838eee6f77911d8cf0f1d908af
Mazinho <mazinho.github@gmail.com>
2023-11-16 11:20:43 -0300

    Project Created

---
+++ src/test/java/com/projetojava/projetojava/ProjetojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojava.projetojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
